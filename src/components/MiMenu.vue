f283ea07d92bd9adbf10df8ce99b28ab83ad251e
Diego Martinez <proyectos@solincosta.com>
2025-08-21 19:56:04 -0500

    PRIMER COMMIT VUE

---
+++ src/components/MiMenu.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <menu class="menu">
+            <li>INICIO</li>
+            <li>ABOUT</li>
+            <li>SERVICE</li>
+            <li>CONTACT</li>
+        </menu>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MiMenu',
+}
+</script>
+
+<style>
+
+.menu{
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 80px;
+    background-color: darkgreen;
+    color: aliceblue;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <MiMenu></MiMenu>
+    <MiMenu></MiMenu>
+    <MiMenu></MiMenu>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import MiMenu from '@/components/MiMenu.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    MiMenu
+  }
+}
+</script>
